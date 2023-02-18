d05e2e1e89e13d36e834c8245f71c9be5ff71a70
DESKTOP-363V6GM\Daniel <kellylisbino@gmail.com>
2023-02-18 17:51:01 -0300

    05 Add getCoin method

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ def getNews():
     for result in results:
         print(result["title"])
 
+def getCoin():
+    cg = CoinGeckoAPI()
+    cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true',
+                 include_24hr_change='true', include_last_updated_at='true')
+    cg.get_coins_markets(vs_currency='usd', ids='bitcoin', price_change_percentage='1h')
+
 if __name__ == '__main__':
-    getNews()
+    #getNews()
+    getCoin()
