a6e361cc7b2cf51eb7ae27f42ebf72ff3307cb8e
DESKTOP-363V6GM\Daniel <kellylisbino@gmail.com>
2023-02-18 17:25:03 -0300

    04 Simplify

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests, json
+from pycoingecko import CoinGeckoAPI
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 def getNews():
     url = 'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&kind=news'
-    news = requests.get(url)
-    data = news.json()
-    results = data["results"]
+    results = requests.get(url).json()["results"]
     for result in results:
         print(result["title"])
 
