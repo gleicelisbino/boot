a52b93f861e4115272ee5b6b8f0228e6758198a0
DESKTOP-363V6GM\Daniel <kellylisbino@gmail.com>
2023-02-18 17:09:30 -0300

    03 Add get title of news

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import requests
+import requests, json
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 def getNews():
-    news = requests.get(
-        'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&kind=news')
-
+    url = 'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&kind=news'
+    news = requests.get(url)
+    data = news.json()
+    results = data["results"]
+    for result in results:
+        print(result["title"])
 
 if __name__ == '__main__':
     getNews()
