880b030db3b6852d7b996d418dc7373a10e9a382
DESKTOP-363V6GM\Daniel <kellylisbino@gmail.com>
2023-02-21 14:44:42 +0000

    09 Add text_blob_tags

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import inline as inline
-import matplotlib
-import numpy as np
 import pandas as pd
-import requests, json, pandas, numpy, nltk
+import requests, numpy, nltk
+import json
 from pycoingecko import CoinGeckoAPI
-import matplotlib.pyplot as plot
 from textblob import TextBlob
 
 def print_hi(name):
     print(f'Hi, {name}')
 #kind=news
 def getNews():
-    url = 'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&currencies=BTC'
+    url = 'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&currencies=BTC&filter=rising'
     results = requests.get(url).json()["results"]
     title = []
     sentiment = []
     for result in results:
         title.append(result["title"])
-        pola = TextBlob(result["title"])
-        if pola.sentiment.polarity > 0:
+        text_blob_title = TextBlob(result["title"])
+        if text_blob_title.sentiment.polarity > 0:
             sentiment.append(1)
-        elif pola.sentiment.polarity == 0:
+        elif text_blob_title.sentiment.polarity == 0:
+            text_blob_tags = text_blob_title.tags
             sentiment.append(0)
         else:
             sentiment.append(-1)
