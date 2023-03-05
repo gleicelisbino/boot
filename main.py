72c9abad6c1b5850ff1bc245aa3b9cff58d682e3
gleicelisbino <kellylisbino@gmail.com>
2023-03-04 23:08:13 -0300

    14 Add model

--- main.py
+++ main.py
@@ -1,37 +1,70 @@
-import pandas as pd
-import time
+import numpy as np
 import yfinance as yf
-from datetime import datetime, timedelta
-from GoogleNews import GoogleNews
+from keras.layers import Bidirectional
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Lambda, Reshape
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import confusion_matrix, accuracy_score
+# Obtém dados históricos do preço do Bitcoin com as colunas Open, High, Low, Close e Volume
+df_price = yf.download('BTC-USD', start='2023-01-01', end='2023-03-03'
+                                                          '', interval='1h', group_by='ticker')
 
-start = datetime.now() - timedelta(days=7)
-end = datetime.now()
-def getNews():
-    url = 'https://cryptopanic.com/api/v1/posts/?auth_token=45a3abde3f2ed8d46084b1edd357ae28c250ca6e&currencies=BTC&filter=rising'
-    results = requests.get(url).json()["results"]
-    title = []
-    sentiment = []
-    for result in results:
-        title.append(result["title"])
-        text_blob_title = TextBlob(result["title"])
-        if text_blob_title.sentiment.polarity > 0:
-            sentiment.append(1)
-        elif text_blob_title.sentiment.polarity == 0:
-            text_blob_tags = text_blob_title.tags
-            sentiment.append(0)
-        else:
-            sentiment.append(-1)
+# Calcula a variação percentual do preço do Bitcoin
+df_price['Variation'] = df_price['Close'].pct_change() * 100
+df_price['Timestamp'] = df_price.index.hour
+# Remove a primeira linha que contém valores NaN
+df_price = df_price.iloc[1:]
 
-    data = pd.DataFrame(data=title, columns=['News'])
-    data['sentiment'] = sentiment
-    print(data)
+# Cria um conjunto de dados de treinamento e teste
+train_size = int(len(df_price) * 0.8)
+train_data = df_price.iloc[:train_size][['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close', 'Variation', 'Timestamp']].values
+test_data = df_price.iloc[train_size:][['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close', 'Variation', 'Timestamp']].values
 
-def getCoin():
-    cg = CoinGeckoAPI()
-    cg.get_price(ids='bitcoin', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true',
-                 include_24hr_change='true', include_last_updated_at='true')
-    cg.get_coins_markets(vs_currency='usd', ids='bitcoin', price_change_percentage='1h')
+# Normaliza os dados de treinamento e teste
+scaler = MinMaxScaler(feature_range=(-1, 1))
+train_data_normalized = scaler.fit_transform(train_data)
+test_data_normalized = scaler.transform(test_data)
 
-if __name__ == '__main__':
-    getNews()
-    #getCoin()
+# Define o número de amostras a serem utilizadas para a predição. 70 é muito bom loss: 0.0019 - val_loss: 0.0012. Testar com 68.
+timesteps = 70
+
+# Cria os dados de treinamento com a janela deslizante
+X_train = []
+y_train = []
+for i in range(timesteps, len(train_data_normalized)):
+    X_train.append(train_data_normalized[i-timesteps:i, :])
+    y_train.append(train_data_normalized[i, :])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Cria os dados de teste com a janela deslizante
+X_test = []
+y_test = []
+for i in range(timesteps, len(test_data_normalized)):
+    X_test.append(test_data_normalized[i-timesteps:i, :])
+    y_test.append(test_data_normalized[i, :])
+X_test, y_test = np.array(X_test), np.array(y_test)
+
+# Reshape dos dados de treinamento e teste
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2])
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2])
+
+model = Sequential()
+model.add(Bidirectional(LSTM(units=50, activation='relu', return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2]))))
+model.add(Bidirectional(LSTM(units=50, activation='relu')))
+model.add(Dense(units=8, activation=None))
+model.compile(optimizer='nadam', loss='mse')
+early_stop = EarlyStopping(monitor='val_loss', patience=3, verbose=1, restore_best_weights=True)
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, callbacks=[early_stop], verbose=1)
+
+last_data = test_data_normalized[-1:]
+
+last_data = np.expand_dims(last_data, axis=0)
+prediction = model.predict(X_test)
+prediction = scaler.inverse_transform(prediction)
+
+y_test = scaler.inverse_transform(y_test)
+
+print("Previsão das seis próximas observações:")
+print(prediction[0])
